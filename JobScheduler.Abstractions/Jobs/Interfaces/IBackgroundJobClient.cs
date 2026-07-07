6a686f24cde3b439c6ae6b650fb7b29a09de49d9
Achi00 <toolvlg@gmail.com>
2026-07-07 23:47:57 +0400

    added BackgroundJobClient

--- JobScheduler.Abstractions/Jobs/Interfaces/IBackgroundJobClient.cs
+++ JobScheduler.Abstractions/Jobs/Interfaces/IBackgroundJobClient.cs
@@ -1,6 +1,6 @@
 ﻿namespace JobScheduler.Abstractions.Jobs.Interfaces
 {
-    internal interface IBackgroundJobClient
+    public interface IBackgroundJobClient
     {
         Task<Guid> EnqueueAsync<TPayload>(TPayload payload, CancellationToken cancellationToken = default);
         Task<Guid> ScheduleAsync<TPayload>(TPayload payload, DateTimeOffset runAt, CancellationToken cancellationToken = default);
---
+++ JobScheduler.Core/BackgroundJobClient.cs
@@ -0,0 +1,56 @@
+﻿using JobScheduler.Abstractions.Jobs.Enums;
+using JobScheduler.Abstractions.Jobs.Interfaces;
+using JobScheduler.Abstractions.Jobs.Structs;
+using JobScheduler.Core.Storage;
+using System.Text.Json;
+
+namespace JobScheduler.Core
+{
+    internal class BackgroundJobClient : IBackgroundJobClient
+    {
+        private readonly IJobStore _jobStore;
+
+        public BackgroundJobClient(IJobStore jobStore)
+        {
+            _jobStore = jobStore;
+        }
+        public async Task<Guid> EnqueueAsync<TPayload>(TPayload payload, CancellationToken cancellationToken = default)
+        {
+            var jobId = JobId.New();
+
+            var job = new JobRecord
+            {
+                Id = jobId.Value,
+                // TODO: later will be using JobNameAttribute
+                JobType = typeof(TPayload).FullName!,
+                PayloadJson = JsonSerializer.Serialize(payload),
+                Status = JobStatus.Enqueued,
+                CreatedAt = DateTimeOffset.UtcNow,
+                NextRunAt = null
+            };
+            await _jobStore.CreateAsync(job, cancellationToken);
+
+            return jobId.Value;
+        }
+
+        public async Task<Guid> ScheduleAsync<TPayload>(TPayload payload, DateTimeOffset runAt, CancellationToken cancellationToken = default)
+        {
+            var jobId = JobId.New();
+
+            var job = new JobRecord
+            {
+                Id = jobId.Value,
+                // TODO: later will be using JobNameAttribute
+                JobType = typeof(TPayload).FullName!,
+                PayloadJson = JsonSerializer.Serialize(payload),
+                Status = JobStatus.Scheduled,
+                CreatedAt = DateTimeOffset.UtcNow,
+                NextRunAt = null
+            };
+
+            await _jobStore.CreateAsync(job, cancellationToken);
+
+            return jobId.Value;
+        }
+    }
+}
