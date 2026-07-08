158980637b0584ef446bcff0fe43884b47841fa4
Achi00 <toolvlg@gmail.com>
2026-07-08 22:49:55 +0400

    create simple bg worker

---
+++ JobScheduler.Core/HostedServices/JobWorkerHostedService.cs
@@ -0,0 +1,27 @@
+﻿using JobScheduler.Core.Execution;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace JobScheduler.Core.HostedServices
+{
+    internal sealed class JobWorkerHostedService : BackgroundService
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+        private readonly string _workerId = $"{Environment.MachineName}-{Guid.NewGuid():N}";
+
+        public JobWorkerHostedService(IServiceScopeFactory scopeFactory)
+        {
+            _scopeFactory = scopeFactory;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await using var scope = _scopeFactory.CreateAsyncScope();
+
+                var processor = scope.ServiceProvider.GetRequiredService<JobProcessor>();
+            }
+        }
+    }
+}
