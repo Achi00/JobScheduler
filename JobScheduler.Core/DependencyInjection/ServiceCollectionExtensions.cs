8029980696508528c6e396d73b57e25282b9654f
Achi00 <toolvlg@gmail.com>
2026-07-08 12:55:17 +0400

    added core layer DI extensions

---
+++ JobScheduler.Core/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using JobScheduler.Abstractions.Jobs.Interfaces;
+using JobScheduler.Core.Execution;
+using JobScheduler.Core.Registry;
+using JobScheduler.Core.Storage;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JobScheduler.Core.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddJobSchedulerCore(this IServiceCollection services)
+        {
+            services.AddSingleton<IJobStore, InMemoryJobStore>();
+            services.AddSingleton<JobRegistry>();
+
+            services.AddScoped<IBackgroundJobClient, BackgroundJobClient>();
+            services.AddScoped<JobProcessor>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddJob<TPayload, THandler>(this IServiceCollection services) 
+            where THandler : class, IJobHandler<TPayload>
+        {
+            var jobType = typeof(TPayload).FullName!;
+
+            services.AddScoped<THandler>();
+
+            services.AddSingleton<IJobExecutor>(
+                new JobExecutor<TPayload, THandler>(jobType));
+
+            return services;
+        }
+    }
+}
