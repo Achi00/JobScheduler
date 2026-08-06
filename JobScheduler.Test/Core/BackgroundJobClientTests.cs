813b72dbc817765736f7fb27965a007648097fcd
Achi00 <toolvlg@gmail.com>
2026-08-06 23:32:09 +0400

    moved on Bg job client testing

---
+++ JobScheduler.Test/Core/BackgroundJobClientTests.cs
@@ -0,0 +1,18 @@
+﻿using JobScheduler.Core.Options;
+using JobScheduler.Storage.Abstractions.Jobs;
+using Microsoft.Extensions.Options;
+using Moq;
+
+namespace JobScheduler.Test.Core
+{
+    public class BackgroundJobClientTests
+    {
+        private readonly Mock<IJobStore> _jobStoreMock;
+        private readonly IOptions<JobSchedulerOptions> _options;
+
+        public BackgroundJobClientTests()
+        {
+            _jobStoreMock = new();
+        }
+    }
+}
