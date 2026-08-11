1f823978937519c3846f2f26ffd247385aecb8dc
Achi00 <toolvlg@gmail.com>
2026-08-11 23:49:33 +0400

    testing, job next run available in past, should set availableAt not null, avoids job to never get claimed

--- JobScheduler.Test/Core/BackgroundJobClientTests.cs
+++ JobScheduler.Test/Core/BackgroundJobClientTests.cs
@@ -254,5 +254,28 @@ namespace JobScheduler.Test.Core
             Assert.NotNull(captured);
             Assert.Equal(runAt, captured.AvailableAt);
         }
+
+        [Fact]
+        public async Task ScheduleAsync_WhenRunAtIsInPast_ShouldSetAvailableAtToNowNotNull()
+        {
+            JobRecord? captured = null;
+
+            _jobStoreMock
+                .Setup(x => x.CreateAsync(
+                        It.IsAny<JobRecord>(),
+                        It.IsAny<CancellationToken>()))
+                .Callback<JobRecord, CancellationToken>((job, _) => captured = job)
+                .Returns(Task.CompletedTask);
+
+            var client = new BackgroundJobClient(_jobStoreMock.Object, Options.Create(_options), _timeProvider);
+
+            // availabe in past
+            var runAt = _timeProvider.GetUtcNow().AddDays(-1);
+
+            await client.ScheduleAsync<SendEmailJob>(new SendEmailJob(Guid.NewGuid(), "welcome"), runAt);
+
+            Assert.NotNull(captured!.AvailableAt);
+            Assert.Equal(_timeProvider.GetUtcNow(), captured.AvailableAt);
+        }
     }
 }
