fc51dac577cc03e85bf7b92f2ad0940d0140b01b
Achi00 <toolvlg@gmail.com>
2026-08-16 22:59:56 +0400

    created migration, updated database

---
+++ JobScheduler.EntityFrameworkCore/Migrations/20260816185725_RecurringJobs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobScheduler.EntityFrameworkCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class RecurringJobs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RecurringJob",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    JobType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PayloadJson = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CronExpression = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TimeZoneId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NextRunAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    LastRunAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RecurringJob", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RecurringJob");
+        }
+    }
+}
