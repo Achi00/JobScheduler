910b8e56c5d1ba3a397b28ee78812d8c8ec39701
Achi00 <toolvlg@gmail.com>
2026-08-16 22:36:51 +0400

    added reecuring job entity, created context dbset

--- JobScheduler.EntityFrameworkCore/Persistence/Context/JobSchedulerDbContext.cs
+++ JobScheduler.EntityFrameworkCore/Persistence/Context/JobSchedulerDbContext.cs
@@ -7,6 +7,7 @@ namespace JobScheduler.EntityFrameworkCore.Persistence.Context
     {
         internal DbSet<JobEntity> Jobs => Set<JobEntity>();
         internal DbSet<JobStateEntity> JobStates => Set<JobStateEntity>();
+        internal DbSet<RecurringJob> RecurringJob => Set<RecurringJob>();
         public JobSchedulerDbContext(DbContextOptions<JobSchedulerDbContext> options) : base(options)
         {
         }
