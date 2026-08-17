95be14cb5efa91de5c240ad024c90387a3f12eec
Achi00 <toolvlg@gmail.com>
2026-08-17 20:13:24 +0400

    testing cronos

---
+++ JobScheduler.Test/Core/CronosSchedulerTests.cs
@@ -0,0 +1,17 @@
+﻿using JobScheduler.Core.Recurring;
+
+namespace JobScheduler.Test.Core
+{
+    public class CronosSchedulerTests
+    {
+        private readonly CronosScheduler _scheduler = new();
+
+        [Fact]
+        public void GetNextOccurrence_WithHourlyExpression_ReturnsNextHourBoundary()
+        {
+            var from = new DateTimeOffset(2026, 1, 1, 10, 15, 0, TimeSpan.Zero);
+            var next = _scheduler.GetNextOccurrence("0 * * * *", "UTC", from);
+            Assert.Equal(new DateTimeOffset(2026, 1, 1, 11, 0, 0, TimeSpan.Zero), next);
+        }
+    }
+}
