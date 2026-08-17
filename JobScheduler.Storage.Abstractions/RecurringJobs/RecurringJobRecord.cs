3551cfe08270fbdd77cc8f7ed57617198114882b
Achi00 <toolvlg@gmail.com>
2026-08-17 23:16:40 +0400

    starting reecuring jobs storace and claim logics

---
+++ JobScheduler.Storage.Abstractions/RecurringJobs/RecurringJobRecord.cs
@@ -0,0 +1,9 @@
+﻿namespace JobScheduler.Storage.Abstractions.RecurringJobs
+{
+    public sealed record RecurringJobRecord(Guid Id,
+    string JobType,
+    string CronExpression,
+    string TimeZoneId,
+    bool IsEnabled,
+    DateTimeOffset? NextRunAt);
+}
