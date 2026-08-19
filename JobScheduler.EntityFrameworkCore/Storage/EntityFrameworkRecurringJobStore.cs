83bcba7b6b8b8fb38b148b48e71b57dc5badbf15
Achi00 <toolvlg@gmail.com>
2026-08-19 22:23:02 +0400

    creating storage/logic singiture for ef core and RecurringJobStore

---
+++ JobScheduler.EntityFrameworkCore/Storage/EntityFrameworkRecurringJobStore.cs
@@ -0,0 +1,28 @@
+﻿using JobScheduler.Storage.Abstractions.RecurringJobs;
+
+namespace JobScheduler.EntityFrameworkCore.Storage
+{
+    public sealed class EntityFrameworkRecurringJobStore : IRecurringJobStore
+    {
+        // follows same idea as IJobStore, server locking
+        public Task AddOrUpdateAsync(RecurringJobRecord job, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<RecurringJobRecord>> GetDueForUpdateAsync(DateTimeOffset now, int batchSize, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(Guid id, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateNextRunAsync(Guid id, DateTimeOffset nextRunAt, DateTimeOffset lastRunAt, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- JobScheduler.Storage.Abstractions/RecurringJobs/IRecurringJobStore.cs
+++ JobScheduler.Storage.Abstractions/RecurringJobs/IRecurringJobStore.cs
@@ -1,5 +1,10 @@
 ﻿namespace JobScheduler.Storage.Abstractions.RecurringJobs
 {
+    /*
+     * follows same principle as IJobstore, in case of ef core also should implament server locking READPAST/UPDLOCK,
+     * so it can behave as expected in case of multiple app instances and will scale horizontally
+     * otherwise they will race for same RecurringJob rows
+    */
     public interface IRecurringJobStore
     {
         Task AddOrUpdateAsync(RecurringJobRecord job, CancellationToken cancellationToken);
