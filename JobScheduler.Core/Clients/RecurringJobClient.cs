7100039248bcdd7d6c417d4fbed66f86af3f9d26
Achi00 <toolvlg@gmail.com>
2026-08-25 14:54:11 +0400

    implamented simplest RecurringJobClient version

--- JobScheduler.Core/Clients/RecurringJobClient.cs
+++ JobScheduler.Core/Clients/RecurringJobClient.cs
@@ -1,17 +1,39 @@
 ﻿using JobScheduler.Abstractions.Jobs.Interfaces;
+using JobScheduler.Core.Options;
+using JobScheduler.Core.Resolvers;
+using JobScheduler.Storage.Abstractions.RecurringJobs;
+using Microsoft.Extensions.Options;
+using System.Text.Json;
 
 namespace JobScheduler.Core.Clients
 {
     internal sealed class RecurringJobClient : IRecurringJobClient
     {
-        public Task AddOrUpdateAsync<TPayload>(Guid recurringJobId, string cronExpression, TPayload payload, CancellationToken cancellationToken = default)
+        private readonly IRecurringJobStore _store;
+
+        public RecurringJobClient(IRecurringJobStore store)
         {
-            throw new NotImplementedException();
+            _store = store;
+        }
+        public async Task AddOrUpdateAsync<TPayload>(Guid recurringJobId, string cronExpression, TPayload payload, CancellationToken cancellationToken = default)
+        {
+            var record = new RecurringJobRecord(
+                Id: recurringJobId,
+                JobType: JobTypeNameResolver.Resolve<TPayload>(),
+                PayloadJson: JsonSerializer.Serialize(payload),
+                CronExpression: cronExpression,
+                TimeZoneId: TimeZoneInfo.Utc.Id,
+                IsEnabled: true,
+                NextRunAt: null,
+                LastRunAt: null
+            );
+
+            await _store.AddOrUpdateAsync(record, cancellationToken);
         }
 
-        public Task RemoveAsync(Guid recurringJobId, CancellationToken cancellationToken = default)
+        public async Task RemoveAsync(Guid recurringJobId, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            await _store.RemoveAsync(recurringJobId, cancellationToken);
         }
     }
 }
