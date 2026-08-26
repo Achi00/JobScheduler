df2f5cc8b33ee9b801d5f45b957e6ff1a6edd0c6
Achi00 <toolvlg@gmail.com>
2026-08-26 20:03:53 +0400

    fix method name type, added json serialization test

--- JobScheduler.Test/Core/RecurringJobClientTests.cs
+++ JobScheduler.Test/Core/RecurringJobClientTests.cs
@@ -5,6 +5,7 @@ using JobScheduler.Core.Resolvers;
 using JobScheduler.Storage.Abstractions.RecurringJobs;
 using Microsoft.Extensions.Time.Testing;
 using Moq;
+using System.Text.Json;
 
 namespace JobScheduler.Test.Core
 {
@@ -21,7 +22,7 @@ namespace JobScheduler.Test.Core
             _timeProvider = new FakeTimeProvider();
         }
 
-        private RecurringJobClient CreateCleint() => new(_storeMock.Object, _cronSchedulerMock.Object, _timeProvider);
+        private RecurringJobClient CreateClient() => new(_storeMock.Object, _cronSchedulerMock.Object, _timeProvider);
 
         [Fact]
         public async Task AddOrUpdateAsync_WhenCalled_ShouldCreateRecordWithComputedNextRunAt()
@@ -41,7 +42,7 @@ namespace JobScheduler.Test.Core
                 )).Callback<RecurringJobRecord, CancellationToken>((record, _) => captured = record)
                 .Returns(Task.CompletedTask);
 
-            var client = CreateCleint();
+            var client = CreateClient();
             var jobId = Guid.NewGuid();
 
             await client.AddOrUpdateAsync(jobId, "0 * * * *", new SendEmailJob(Guid.NewGuid(), "welcome"), "UTC");
@@ -53,6 +54,32 @@ namespace JobScheduler.Test.Core
             Assert.True(captured.IsEnabled);
             Assert.Equal(expectedNextRunAt, captured.NextRunAt);
             Assert.Null(captured.LastRunAt);
+        }
+
+
+        [Fact]
+        public async Task AddOrUpdateAsync_ShouldSerializePayload()
+        {
+            _cronSchedulerMock
+                .Setup(x => x.GetNextOccurrence(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<DateTimeOffset>()))
+                .Returns(_timeProvider.GetUtcNow().AddHours(1));
+
+            RecurringJobRecord? captured = null;
+            _storeMock
+                .Setup(x => x.AddOrUpdateAsync(It.IsAny<RecurringJobRecord>(), It.IsAny<CancellationToken>()))
+                .Callback<RecurringJobRecord, CancellationToken>((record, _) => captured = record)
+                .Returns(Task.CompletedTask);
+
+            var client = CreateClient();
+            var originalPayload = new SendEmailJob(Guid.NewGuid(), "welcome");
+
+            await client.AddOrUpdateAsync(Guid.NewGuid(), "0 * * * *", originalPayload, "UTC");
+
+            Assert.NotNull(captured);
+            Assert.Equal(JobTypeNameResolver.Resolve<SendEmailJob>(), captured!.JobType);
+
+            var deserialized = JsonSerializer.Deserialize<SendEmailJob>(captured.PayloadJson);
+            Assert.Equal(originalPayload, deserialized);
         }
     }
 }
