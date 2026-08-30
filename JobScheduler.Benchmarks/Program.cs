f54c61d7d6fa4d2b94443ccda3ff26273b40c93f
Achi00 <toolvlg@gmail.com>
2026-08-30 22:45:43 +0400

    wire up di container for benchmark app

--- JobScheduler.Benchmarks/Program.cs
+++ JobScheduler.Benchmarks/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using JobScheduler.Abstractions.Jobs.Interfaces;
+using JobScheduler.Benchmarks.Handlers;
+using JobScheduler.Benchmarks.Models;
+using JobScheduler.Core.DependencyInjection;
+using JobScheduler.Storage.SqlServer.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services
+    .AddJobScheduler()
+    .AddServer();
+
+builder.Services.AddSqlServerJobStorage("Server=localhost;Database=JobScheduler;Trusted_Connection=True;TrustServerCertificate=True");
+
+builder.Services.AddJob<NoOpJob, NoOpJobHandler>();
+
+var services = new ServiceCollection();
+
+using var host = builder.Build();
+
+await host.StartAsync();
+
+var client = host.Services.GetRequiredService<IBackgroundJobClient>();
+
+Console.WriteLine("Benchmark host started.");
+
+await host.StopAsync();
